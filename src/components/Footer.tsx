35f9810b5330b02eb67509531fefede3e3ade40e
Matt P <mpoteshman1@gmail.com>
2025-03-02 17:27:45 -0600

    Centering footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   // const teamNumber = useQRScoutState(state => state.formData.teamNumber);
   return (
     <footer>
-      <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
+      <div className="mt-8 flex flex-col items-center justify-center text-center m-8 p-2 gap-2">
       ComboScout v25.1 | Based on QRScout by Team 2713
         {/* <div className="h-24 w-96">
           <Logo />
